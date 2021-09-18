6ab763a0ab9e2be61552bd7aacd1eb95a75b40aa
Abby Castelow <abby.castelow@gmail.com>
2021-09-17 19:13:01 -0600

    first upload

---
+++ Assets/README.md
@@ -0,0 +1,47 @@
+# Note Taker
+  
+  ## Table of Contents
+  [Description](#description)  
+  [Installation](#installation)  
+  [Usage](#usage)  
+  [License](#license)  
+  [Tests](#tests)  
+  [Contributing](#contributing)  
+  [Questions](#questions)
+
+  ## Description
+  We modified the given starter code to create an application called Note Taker that can be used to write and save notes. This application uses an Express.js back end and save and retrieve note data from a JSON file.
+
+  ## Installation
+  Run
+  > npm i
+  
+  to pull in dependencies.
+
+  ## Usage
+  Run
+  > node server.js
+  
+  to run the program.
+
+  ## Tests
+  Run
+  > npm run test
+  
+  to test the program
+  
+  ## License
+    
+  MIT 
+    
+  https://opensource.org/licenses/MIT  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Contributing
+  Contact abby.castelow@gmail.com to a get a pull request.
+
+  ## Questions
+  Contact abby.castelow@gmail.com for any questions.
+  Github username: Bodheim
+  Link to my github profile: undefined
+  
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+const { randomUUID } = require("crypto");
+const util = require("util");
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const writeToFile = util.promisify(fs.writeFile);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.send(data);
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  let newNote = {};
+  if (title && text) {
+    newNote = {
+      title,
+      text,
+      id: randomUUID(),
+    };
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      const allNotes = [].concat(JSON.parse(data));
+      allNotes.push(newNote);
+      console.log(typeof data);
+      console.log(allNotes);
+      writeToFile("./db/db.json", JSON.stringify(allNotes), (err) => {
+        err ? console.log(err) : console.log("Success. File was written!");
+      })
+        .then(
+          res.status(201).sendFile(path.join(__dirname, "./public/notes.html"))
+        )
+        .catch(err ? console.log(err) : console.log("Woo!"));
+    });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
